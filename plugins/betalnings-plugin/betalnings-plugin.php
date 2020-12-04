b092b174b8ad9b14992d8eba7de7c2150a7aa123
popka56 <popka56@github.com>
2020-12-04 11:08:19 +0100

    Byggt om betalningsplugin från grunden; frontend

--- plugins/betalnings-plugin/betalnings-plugin.php
+++ plugins/betalnings-plugin/betalnings-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Slutprojekt - Betalnings Plugin
 Plugin URI: 
-description:
+description: Lägger till en ny betalningsmetod för woocommerce; Faktura via personnummer.
 Version: 1.0
 Author: Elin, Hugo & Kristoffer
 Author URI: 
@@ -14,81 +14,78 @@ if(!defined('ABSPATH')){
    die;
 }
 
-class EHKPayment
-{
-   //Lite förberedelser
-   function __construct() {
-      add_filter( 'woocommerce_review_order_before_payment', array( $this, 'create_ehk_payment_form' ), 10, 1 ); //TODO: Hitta rätt filter att använda, kanske lägg till direkt till wooceommerce payment gateways?
-      wp_enqueue_script( 'script', plugin_dir_url( __FILE__ ) . 'js/betalning.js');
-   }
+//Hooks
+add_filter( 'woocommerce_payment_gateways', 'add_ehk_payment_option' );
+function add_ehk_payment_option( $gateways ) {
+	$gateways[] = 'EHK_Payment'; //Klassnamnet
+	return $gateways;
+}
+add_action( 'plugins_loaded', 'init_ehk_payment_option' );
+function init_ehk_payment_option() {
+ 
+   //Här bygger vi vår betalningsmetod baserat på woocommerces payment gateway class
+   class EHK_Payment extends WC_Payment_Gateway {
 
-   //Skapa tables
-   function on_plugin_activate() {
-      global $wpdb;
-      $table_name = $wpdb->prefix . "ehk_bills";
-      
-      //kolla om ehk_bills redan finns
-      $query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
-      if (!$wpdb->get_var($query) == $table_name){
-         //skapa ett table
-         $charset_collate = $wpdb->get_charset_collate();
+      public function __construct() {
+         //Sätter lite standardvärden
+         $this->id = 'ehk';
+         $this->has_fields = true;
+         $this->method_title = 'Faktura'; //Titeln i woocommerces "Payment" inställning
+         $this->method_description = 'Skickar faktura via ett giltigt personnummer.'; //Beskrivningen i woocommerces "Payment" inställning
+         $this->supports = array(
+            'products'
+         );
+         
+         //Ladda våra standardvärden
+         $this->init_form_fields();
+         $this->init_settings();
+         $this->title = $this->get_option( 'title' );
+         $this->description = $this->get_option( 'description' );
+         $this->enabled = $this->get_option( 'enabled' );
+         
+         //Spara med en hook
+         add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 
-         $sql = "CREATE TABLE $table_name (
-           id mediumint(9) NOT NULL AUTO_INCREMENT,
-           time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-           email text NOT NULL,
-           paid int DEFAULT 0 NOT NULL,
-           PRIMARY KEY  (id)) 
-           $charset_collate;";
-
-         $path = preg_replace('/wp-content.*$/', '', __DIR__);
-         require_once($path.'wp-admin/includes/upgrade.php');
-         dbDelta($sql);
+         
       }
-      flush_rewrite_rules();
-   }
 
-   //Ta bort den sparade väder platsen och databasen för kontaktformuläret vid avinstallation
-   function on_plugin_uninstall(){
-      global $wpdb;
-      $table_name = $wpdb->prefix . 'ehk_bills';
-      $sql = "DROP TABLE IF EXISTS $table_name";
-      $wpdb->query($sql);
-   }
-
-   //Skapa själva input:en
-   public function create_ehk_payment_form(){      
-      echo '
-      <div>
-         <h2>Faktura med mejl</h2>
-         <span id="response"></span>
-            <label for="email">E-Mail fakturan ska skickas till:</label>
-            <input type="text" id="email" required>
-            <br>
-            <input type="submit" value="Submit" onclick="submit_form()">
-      </div>';
-   }
-
-   //Skriv ut en lista med alla utskick
-   public static function get_ehk_bills(){
-      global $wpdb;
-      $results = $wpdb->get_results( 
-                  $wpdb->prepare("SELECT * FROM {$wpdb->prefix}ehk_bills LIMIT 10") 
-               );
-      echo '<ul>';
-      foreach($results as $result){
-         echo  '<li>' . 
-               'Skickad: ' . $result->time . '<br>' .
-               'Till: ' . $result->email . '<br>' .
-               'Betald: ' . $result->paid . //TODO: lägg en if-sats för att skriva Ja eller Nej istället för 0 eller 1 
-               '</li>';
+      //Inställningssidan för betalningsmetoden på woocommerces payment sida
+      public function init_form_fields(){
+         $this->form_fields = array(
+            'enabled' => array(
+               'title'       => 'Aktivera/Avaktivera',
+               'label'       => 'Aktivera faktura via personnummer',
+               'type'        => 'checkbox',
+               'description' => '',
+               'default'     => 'no'
+            ),
+            'title' => array(
+               'title'       => 'Title',
+               'type'        => 'text',
+               'description' => 'Namnet på betalningsmetoden kunden ser i checkout.',
+               'default'     => 'Faktura',
+               'desc_tip'    => true,
+            ),
+            'description' => array(
+               'title'       => 'Description',
+               'type'        => 'textarea',
+               'description' => 'Beskrivningen som kunden ser i checkout.',
+               'default'     => 'Skriv in ditt personnummer så skickar vi en faktura till dig.',
+            )
+         );
 
       }
-      echo '</ul>';
+
+      //Formuläret som visas i checkout
+      public function payment_fields() {
+         echo '
+         <p>' . $this->description . '</p>
+         <fieldset id="wc-' . esc_attr( $this->id ) . '-faktura-form" class="wc-payment-form" style="background:transparent;">
+            <div class="form-row form-row-wide">
+               <label>Personnummer <span class="required">*</span></label>
+               <input id="ehk_pnr" type="text" autocomplete="off">
+            </div>
+         </fieldset>';
+      }
    }
 }
-
-//Registrering-hooks
-$ehkPayment = new EHKPayment();
-register_activation_hook(__FILE__, array($ehkPayment, 'on_plugin_activate'));
-register_activation_hook(__FILE__, array($ehkPayment, 'on_plugin_uninstall'));
